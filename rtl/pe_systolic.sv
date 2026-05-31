57bd8c56f9933bb5e411743f42f5d077b13f0ae7
Gargi Mrinal <gargimrinal6@gmail.com>
2026-05-31 09:22:54 +0530

    Refactor pe_systolic module to include psum_bus

--- rtl/pe_systolic.sv
+++ rtl/pe_systolic.sv
@@ -18,62 +18,48 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module pe_systolic #(
+odule pe_systolic #(
     parameter data_width = 8,
     parameter acc_width  = 2*data_width+4,
     parameter ROWS = 2,
     parameter COLS = 2
 )(
-
     input logic clk,
     input logic rst_n,
     input logic en,
 
-    // left-side activation inputs
     input logic signed [data_width-1:0] a_left [ROWS-1:0],
+    input logic signed [data_width-1:0] w_top  [COLS-1:0],
 
-    // top-side weight inputs
-    input logic signed [data_width-1:0] w_top [COLS-1:0],
-
-    // outputs
     output logic signed [acc_width-1:0] y [ROWS-1:0][COLS-1:0]
 );
 
-
-// internal systolic buses
-
 logic signed [data_width-1:0] a_bus [ROWS-1:0][COLS:0];
-
 logic signed [data_width-1:0] w_bus [ROWS:0][COLS-1:0];
 
+logic signed [acc_width-1:0] psum_bus [ROWS-1:0][COLS:0];
 
-// inject left activations
 genvar r,c;
 
 generate
-
     for(r = 0; r < ROWS; r++) begin
-
         assign a_bus[r][0] = a_left[r];
-
     end
-
 endgenerate
 
-
-// inject top weights
 generate
-
     for(c = 0; c < COLS; c++) begin
-
         assign w_bus[0][c] = w_top[c];
-
     end
-
 endgenerate
-// PE grid
-generate
 
+generate
+    for(r = 0; r < ROWS; r++) begin
+        assign psum_bus[r][0] = '0;
+    end
+endgenerate
+
+generate
     for(r = 0; r < ROWS; r++) begin : ROW_GEN
 
         for(c = 0; c < COLS; c++) begin : COL_GEN
@@ -90,15 +76,30 @@ generate
                 .a_in(a_bus[r][c]),
                 .w_in(w_bus[r][c]),
 
-                .psum_in('0),
+                .psum_in(psum_bus[r][c]),
 
                 .a_out(a_bus[r][c+1]),
                 .w_out(w_bus[r+1][c]),
 
-                .psum_out(y[r][c])
-            );
-        end
-     end
-endgenerate
-endmodule
+                .psum_out(psum_bus[r][c+1])
 
+            );
+
+        end
+
+    end
+endgenerate
+
+generate
+    for(r = 0; r < ROWS; r++) begin : OUT_ASSIGN
+
+        for(c = 0; c < COLS; c++) begin
+
+            assign y[r][c] = psum_bus[r][c+1];
+
+        end
+
+    end
+endgenerate
+
+endmodule
