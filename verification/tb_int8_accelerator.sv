0341700e3f847295a7db9f8e0a26d90b1b166843
Gargi Mrinal <gargimrinal6@gmail.com>
2026-06-09 14:23:17 +0530

    Add files via upload

---
+++ verification/tb_int8_accelerator.sv
@@ -0,0 +1,202 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.06.2026 09:53:00
+// Design Name: 
+// Module Name: tb_int8_accelerator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module tb_int8_accelerator;
+
+parameter data_width = 8;
+parameter acc_width  = 2*data_width+4;
+parameter ROWS = 4;
+parameter COLS = 4;
+
+logic clk;
+logic rst_n;
+logic start;
+
+logic signed [data_width-1:0] a_left [ROWS-1:0];
+logic signed [data_width-1:0] w_top  [COLS-1:0];
+logic done;
+logic signed [acc_width-1:0] y [ROWS-1:0][COLS-1:0];
+
+int8_accelerator #(
+.data_width(data_width),
+.acc_width(acc_width),
+.ROWS(ROWS),
+.COLS(COLS)
+) dut (
+.clk(clk),
+.rst_n(rst_n),
+.start(start),
+.a_left(a_left),
+.w_top(w_top),
+.done(done),
+.y(y)
+);
+// self checking
+task check_reset;
+
+integer i;
+integer j;
+
+begin
+for(i=0; i<ROWS; i=i+1) begin
+    for(j=0; j<COLS; j=j+1) begin
+
+        if(y[i][j] !== 0)
+            $error("FAIL: Reset y[%0d][%0d]=%0d",
+                   i,j,y[i][j]);
+
+    end
+end
+if(done !== 0)
+    $error("FAIL: done not cleared by reset");
+else
+    $display("PASS: Reset Check");
+end
+
+endtask
+
+task check_done;
+
+begin
+ if(done !== 1'b1)
+        $error("FAIL: done not asserted");
+    else
+        $display("PASS: done asserted");
+
+end
+
+endtask
+
+task check_outputs_valid;
+
+integer i;
+integer j;
+
+begin
+for(i=0; i<ROWS; i=i+1) begin
+    for(j=0; j<COLS; j=j+1) begin
+
+        if(^y[i][j] === 1'bx)
+            $error("FAIL: y[%0d][%0d] contains X/Z",
+                   i,j);
+
+    end
+end
+
+$display("PASS: Output Matrix Valid");
+
+end
+
+endtask
+
+initial begin
+clk = 0;
+forever #5 clk = ~clk;
+end
+
+initial begin
+rst_n = 0;
+start = 0;
+
+a_left[0] = 0;
+a_left[1] = 0;
+a_left[2] = 0;
+a_left[3] = 0;
+
+w_top[0] = 0;
+w_top[1] = 0;
+w_top[2] = 0;
+w_top[3] = 0;
+
+#1;
+check_reset();
+
+#9;
+rst_n = 1;
+
+#10;
+
+$display("Running Test 1");
+
+a_left[0] = 1;
+a_left[1] = 2;
+a_left[2] = 3;
+a_left[3] = 4;
+
+w_top[0] = 5;
+w_top[1] = 6;
+w_top[2] = 7;
+w_top[3] = 8;
+
+start = 1;
+
+#10;
+start = 0;
+
+@(posedge done);
+check_done();
+check_outputs_valid();
+
+#20;
+
+$display("Running Test 2");
+
+a_left[0] = -1;
+a_left[1] = -2;
+a_left[2] = 3;
+a_left[3] = 4;
+
+w_top[0] = 2;
+w_top[1] = -3;
+w_top[2] = 4;
+w_top[3] = -5;
+
+start = 1;
+
+#10;
+start = 0;
+
+@(posedge done);
+check_done();
+check_outputs_valid();
+
+#20;
+
+$display("TOP LEVEL SELF-CHECKING TB COMPLETED");
+
+$finish;
+
+end
+
+initial begin
+$monitor(
+    "T=%0t start=%0b done=%0b y00=%0d y01=%0d y10=%0d y11=%0d",
+    $time,
+    start,
+    done,
+    y[0][0],
+    y[0][1],
+    y[1][0],
+    y[1][1]
+);
+end
+
+endmodule
+
---
+++ verification/tb_pe_systolic_4x4.sv
@@ -0,0 +1,177 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 31.05.2026 08:36:36
+// Design Name: 
+// Module Name: tb_pe_systolic_4x4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module tb_pe_systolic_4x4;
+
+parameter data_width = 8;
+parameter acc_width  = 2*data_width + 4;
+parameter ROWS = 4;
+parameter COLS = 4;
+
+logic clk;
+logic rst_n;
+logic en;
+
+// inputs
+logic signed [data_width-1:0] a_left [ROWS-1:0];
+logic signed [data_width-1:0] w_top  [COLS-1:0];
+
+// outputs
+logic signed [acc_width-1:0] y [ROWS-1:0][COLS-1:0];
+
+pe_systolic #(
+    .data_width(data_width),
+    .acc_width(acc_width),
+    .ROWS(ROWS),
+    .COLS(COLS)
+) dut (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en(en),
+    .a_left(a_left),
+    .w_top(w_top),
+    .y(y)
+);
+//checking
+task check_zero_matrix;
+
+integer i;
+integer j;
+
+begin
+
+    for(i = 0; i < ROWS; i = i + 1) begin
+        for(j = 0; j < COLS; j = j + 1) begin
+
+            if(y[i][j] !== 0)
+                $error("FAIL: y[%0d][%0d] = %0d",
+                       i, j, y[i][j]);
+
+        end
+    end
+
+    $display("PASS: Zero Matrix Test");
+
+end
+
+endtask
+task test1_banner;
+begin
+$display("Running Test 1 : Positive Values");
+end
+endtask
+
+task test2_banner;
+begin
+$display("Running Test 2 : Different Positive Values");
+end
+endtask
+
+task test3_banner;
+begin
+$display("Running Test 3 : Signed Values");
+end
+endtask
+// Clock
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+initial begin
+
+    rst_n = 0;
+    en    = 0;
+
+    a_left[0] = 0;
+    a_left[1] = 0;
+    a_left[2] = 0;
+    a_left[3] = 0;
+
+    w_top[0] = 0;
+    w_top[1] = 0;
+    w_top[2] = 0;
+    w_top[3] = 0;
+
+    // Reset
+    #10;
+    rst_n = 1;
+    en    = 1;
+    #20;
+check_zero_matrix();
+  
+    // Test 1
+
+    #40;
+test1_banner();
+    a_left[0] = 1;
+    a_left[1] = 2;
+    a_left[2] = 3;
+    a_left[3] = 4;
+
+    w_top[0] = 5;
+    w_top[1] = 6;
+    w_top[2] = 7;
+    w_top[3] = 8;
+    // Test 2
+      #40;
+test2_banner();
+    a_left[0] = 2;
+    a_left[1] = 4;
+    a_left[2] = 6;
+    a_left[3] = 8;
+
+    w_top[0] = 1;
+    w_top[1] = 2;
+    w_top[2] = 3;
+    w_top[3] = 4;
+
+    // Test 3
+     #40;
+test3_banner();
+    a_left[0] = -1;
+    a_left[1] = -2;
+    a_left[2] = 3;
+    a_left[3] = 4;
+
+    w_top[0] = 2;
+    w_top[1] = -3;
+    w_top[2] = 4;
+    w_top[3] = -5;
+
+    #80;
+
+    $finish;
+
+end
+// Monitor
+
+initial begin
+
+    $monitor(
+        "T=%0t | y00=%0d y01=%0d y02=%0d y03=%0d | y10=%0d y11=%0d y12=%0d y13=%0d",
+        $time,
+        y[0][0], y[0][1], y[0][2], y[0][3],
+        y[1][0], y[1][1], y[1][2], y[1][3]
+    );
+
+end
+
+endmodule
