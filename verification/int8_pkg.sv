db6bd63fc11ef1b0b8a2f48365d57f3436310844
Gargi Mrinal <gargimrinal6@gmail.com>
2026-06-21 15:26:13 +0530

    Add files via upload

---
+++ verification/int8_pkg.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.06.2026 15:01:28
+// Design Name: 
+// Module Name: int8_pkg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+package int8_pkg;
+
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+    `include "int8_seq_item.sv"
+
+endpackage
